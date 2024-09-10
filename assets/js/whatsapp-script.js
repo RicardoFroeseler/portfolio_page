bd2c718e6f22ec3c0e37a78e4e4eadec75a4476c
Ricardo Froeseler <42811079+RicardoFroeseler@users.noreply.github.com>
2024-09-10 19:45:49 -0300

    Add files via upload

--- assets/js/whatsapp-script.js
+++ assets/js/whatsapp-script.js
@@ -15,3 +15,16 @@ function closePopup() {
         modal.style.display = 'none';  // Esconde o modal após a transição
     }, 300);  // O tempo aqui deve ser o mesmo que o tempo da transição em CSS (0.3s = 300ms)
 }
+
+// Função para enviar mensagem pelo WhatsApp
+function sendMessage() {
+    const message = document.getElementById('whatsappMessage').value;  // Captura o texto da mensagem
+    if (message.trim() !== "") {  // Verifica se a mensagem não está vazia
+        const phoneNumber = "5531985266186";  // Substitua pelo número do WhatsApp (inclua o código do país)
+        const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;  // Gera o link para WhatsApp com a mensagem
+        window.open(url, '_blank');  // Abre o WhatsApp em uma nova aba
+        closePopup();  // Fecha o modal após o envio
+    } else {
+        alert("Por favor, digite uma mensagem.");  // Alerta caso o campo de mensagem esteja vazio
+    }
+}
